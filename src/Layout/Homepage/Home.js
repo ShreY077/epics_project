a99231bf9421e95234e36da6bdb940384f90d477
ShreY077 <109760811+ShreY077@users.noreply.github.com>
2024-05-07 01:16:04 +0530

    all done just add history and Members page
    need to add members page as im not able to root that page rest all is done.

--- src/Layout/Homepage/Home.js
+++ src/Layout/Homepage/Home.js
@@ -10,11 +10,12 @@ function Home() {
             <img className= "h_img" src={home} alt="blob"/>
           </div>
           <div className='c2'>
-            <div className='dialuge'> MAKING LIFES BETTER. </div>
+            <div className='dialogue'>MAKING LIFES BETTER.</div>
+            <a href="/Walls" className='get-started-btn'>Get Started</a>
           </div>
         </div>
     </>
   )
-}   
+}
 
 export default Home
